42f1c1e4875384d27ffabadffac8685b1e2112e7
ŠimonČanecký <simoncanecky1@gmail.com>
2020-10-08 09:27:22 +0200

    Snail Goes up the Stairs

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,1 +1,5 @@
-console.log("Hello AppsLab 2020!")
+let numberHeight = 0.3;
+let numberLength = 0.2;
+let numberTower = 25.0;
+let totalDistance = (numberTower / numberHeight) * (numberHeight + numberLength);
+console.log(totalDistance)
---
+++ out/helloAppslabTs.js
@@ -0,0 +1,6 @@
+var numberHeight = 0.3;
+var numberLength = 0.2;
+var numberTower = 25.0;
+var totalDistance = (numberTower / numberHeight) * (numberHeight + numberLength);
+console.log(totalDistance);
+//# sourceMappingURL=helloAppslabTs.js.map
