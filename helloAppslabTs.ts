21c7e9c8a3e78de707b423a1d7403b7edf6f80a0
SimonCanecky <simoncanecky1@gmail.com>
2020-10-19 15:43:41 +0200

    Multiply by Length

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,16 +1,11 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+let cisla = [2, 3, 1, 0];
 
-let a = getRandomInt(10);         
-let b = getRandomInt(10);
-let c = getRandomInt(10);
+cisla = multipleByLenght();
+console.log(cisla);
 
-if (a + b > c && a + c > b && b + c > a){
-    console.log("Hodnoty: " + a + ", " + b + ", " + c);
-    console.log("Trojuhlonik: true");
-}
-else{
-    console.log("Hodnoty: " + a + ", " + b + ", " + c);
-    console.log("Trojuhlonik: false");
+function multipleByLenght() {
+    for (let number = 0; number < cisla.length; number++) {
+        cisla[number] = cisla[number] * cisla.length;
+    }
+    return cisla;
 }
