c0bee1df09877735686f04ce4f728a2b6c7d2e52
SimonCanecky <simoncanecky1@gmail.com>
2020-10-19 15:03:59 +0200

    Equality of 3 values

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,1 +1,35 @@
-console.log("Hello AppsLab 2020!")
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+let a = getRandomInt(5);         //trochu zložitejšie cez random
+let b = getRandomInt(5);
+let c = getRandomInt(5);
+
+console.log("Random cislo a: " + a);
+console.log("Random cislo b: " + b);
+console.log("Random cislo c: " + c);
+
+
+if (a == b) {
+    if (a == c) {
+        console.log("Rovnake su: 3");
+    } else if (b != c) {
+        console.log("Rovnake su: 2");
+    }
+}
+
+else if (b == c) {
+    if (a != c) {
+        console.log("Rovnake su: 2");
+    }
+}
+else if (a == c) {
+    if (b != c) {
+        console.log("Rovnake su: 2");
+    }
+}
+else {
+    console.log("Rovnake su: 0");
+
+}
