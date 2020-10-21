a8e94a927a3fc9613979a4a55d52056dd3fd8e65
SimonCanecky <simoncanecky1@gmail.com>
2020-10-21 10:18:47 +0200

    Slice of Pie

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,1 +1,6 @@
-console.log("Hello AppsLab 2020!")
+console.log("(11, 5, 2)");
+console.log(equalSlices(11,5,2));
+
+function equalSlices(totalSlices : number, noRecipients : number , slicesEach : number) : boolean {
+return noRecipients * slicesEach <= totalSlices;
+}
---
+++ out/helloAppslabTs.js
@@ -0,0 +1,6 @@
+console.log("(11, 5, 2)");
+console.log(equalSlices(11, 5, 2));
+function equalSlices(totalSlices, noRecipients, slicesEach) {
+    return noRecipients * slicesEach <= totalSlices;
+}
+//# sourceMappingURL=helloAppslabTs.js.map
